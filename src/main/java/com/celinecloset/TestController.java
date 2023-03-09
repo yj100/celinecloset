4c818974a609940673f9624eaab57c38583c793f
고객지원_조연지 <gdduswl96@gmail.com>
2023-03-09 12:26:11 +0900

    처음 커밋

---
+++ src/main/java/com/celinecloset/TestController.java
@@ -0,0 +1,17 @@
+package com.celinecloset;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping(value = "/test")
+    public UserDto test() {
+        UserDto userDto = new UserDto();
+        userDto.setAge(20);
+        userDto.setName("hoon");
+
+        return userDto;
+    }
+}
---
+++ src/test/java/com/celinecloset/CelineclosetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celinecloset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelineclosetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
