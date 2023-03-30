b48ce5894cf513f559a809254593cb5edf28eb5c
조연지 <gdduswl96@gmail.com>
2023-03-30 15:44:49 +0900

    MySQL 연동

--- src/main/java/com/celinecloset/CelineclosetApplication.java
+++ src/main/java/com/celinecloset/CelineclosetApplication.java
@@ -2,21 +2,14 @@ package com.celinecloset;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
 @SpringBootApplication
 public class CelineclosetApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(CelineclosetApplication.class, args);
-	}
-
-	@GetMapping(value = "/")
-	public String HelloWorld() {
-		return "HelloWorld";
 	}
 
 }
