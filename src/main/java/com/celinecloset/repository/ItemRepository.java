b93a52e4e5b53ff8254abcddc22481cba537f5d9
권효진 <C:/Program Files/Git/irisnari22@gmail.com>
2023-04-06 15:36:35 +0900

    쿼리 메소드 - 쿼리 메소드 이용 상품 조회

--- src/main/java/com/celinecloset/repository/ItemRepository.java
+++ src/main/java/com/celinecloset/repository/ItemRepository.java
@@ -3,5 +3,9 @@ package com.celinecloset.repository;
 import com.celinecloset.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByItemNm(String itemNm);
+
 }
