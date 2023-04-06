3ea548b5acfe3c6a3b4d66cb384d6351bab52011
조연지 <gdduswl96@gmail.com>
2023-04-06 16:07:47 +0900

    가격 LessThan 테스트 결과 조회된 상품 리스트

--- src/main/java/com/celinecloset/repository/ItemRepository.java
+++ src/main/java/com/celinecloset/repository/ItemRepository.java
@@ -7,5 +7,8 @@ import java.util.List;
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
     List<Item> findByItemNm(String itemNm);
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+
+    List<Item> findByPriceLessThan(Integer price);
 
 }
--- src/test/java/com/celinecloset/repository/ItemRepositoryTest.java
+++ src/test/java/com/celinecloset/repository/ItemRepositoryTest.java
@@ -58,4 +58,25 @@ class ItemRepositoryTest {
         }
     }
 
+    @Test
+    @DisplayName("상품명, 상품상세설명 or 테스트")
+    public void findByItemNmOrItemDetailTest(){
+        this.createItemList();
+        List<Item> itemList =
+itemRepository.findByItemNmOrItemDetail("테스트 상품1", "테스트 상품 상세 설명5");
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("가격 LessThan 테스트")
+    public void findByPriceLessThanTest(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByPriceLessThan(10005);
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+
 }
