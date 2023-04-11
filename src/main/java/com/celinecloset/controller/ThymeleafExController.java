99c217f48cc4201c051950d5a6b204e16b39399d
권효진 <C:/Program Files/Git/irisnari22@gmail.com>
2023-04-11 15:23:23 +0900

    th:each를 이용 - 상품 리스트 출력 예제

--- src/main/java/com/celinecloset/controller/ThymeleafExController.java
+++ src/main/java/com/celinecloset/controller/ThymeleafExController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.celinecloset.dto.ItemDto;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping(value="/thymeleaf")
@@ -30,4 +32,27 @@ public class ThymeleafExController {
         return "thymeleafEx/thymeleafEx02";
 
     }
+
+    @GetMapping(value = "/ex03")
+    public String thymeleafExample03(Model model){
+
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for(int i=1; i<=10; i++){
+
+            ItemDto itemDto = new ItemDto();
+            itemDto.setItemDetail("상품 상세 설명"+i);
+            itemDto.setItemNm("테스트 상품" + i);
+            itemDto.setPrice(1000*i);
+            itemDto.setRegTime(LocalDateTime.now());
+
+            itemDtoList.add(itemDto);
+
+        }
+
+        model.addAttribute("itemDtoList", itemDtoList);
+        return "thymeleafEx/thymeleafEx03";
+
+    }
+
 }
