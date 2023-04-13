587f1724dc81de29a0b69f9ea1c819ffd0b20f8a
권효진 <C:/Program Files/Git/irisnari22@gmail.com>
2023-04-13 10:06:57 +0900

    회원 가입 페이지 작성하기

---
+++ src/main/java/com/celinecloset/controller/MemberController.java
@@ -0,0 +1,37 @@
+package com.celinecloset.controller;
+
+import com.celinecloset.dto.MemberFormDto;
+import com.celinecloset.entity.Member;
+import com.celinecloset.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/members")
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
+
+    @GetMapping(value = "/new")
+    public String memberForm(Model model){
+        model.addAttribute("memberFormDto", new MemberFormDto());
+        return "member/memberForm";
+    }
+
+    @RequestMapping("/members")
+    public String memberForm(MemberFormDto memberFormDto){
+
+        Member member = Member.createMember(memberFormDto, passwordEncoder);
+        memberService.saveMember(member);
+
+        return "redirect:/";
+
+    }
+
+}
