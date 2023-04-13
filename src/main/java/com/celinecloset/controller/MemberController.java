dfee3feb95d48e7d844523f7b27121f014ea38f4
조연지 <gdduswl96@gmail.com>
2023-04-13 10:41:05 +0900

    회원 가입 성공

--- src/main/java/com/celinecloset/controller/MemberController.java
+++ src/main/java/com/celinecloset/controller/MemberController.java
@@ -8,7 +8,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.validation.BindingResult;
+import javax.validation.Valid;
 
 @RequestMapping("/members")
 @Controller
@@ -34,4 +38,23 @@ public class MemberController {
 
     }
 
+    @PostMapping(value = "/new")
+    public String newMember(@Valid MemberFormDto memberFormDto,
+                            BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "member/memberForm";
+        }
+
+
+        try {
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+        } catch (IllegalStateException e){
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/memberForm";
+        }
+
+        return "redirect:/";
+    }
 }
