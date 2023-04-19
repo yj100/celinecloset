a8f61686d641de6f85897a8bad2c697542af1535
조연지 <gdduswl96@gmail.com>
2023-04-19 11:56:57 +0900

    상품 등록하기

--- src/main/java/com/celinecloset/controller/ItemController.java
+++ src/main/java/com/celinecloset/controller/ItemController.java
@@ -3,11 +3,14 @@ package com.celinecloset.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.ui.Model;
+import com.celinecloset.dto.ItemFormDto;
 @Controller
 public class ItemController {
 
     @GetMapping(value = "/admin/item/new")
-    public String itemForm(){
+    public String itemForm(Model model){
+        model.addAttribute("itemFormDto", new ItemFormDto());
         return "/item/itemForm";
     }
 
