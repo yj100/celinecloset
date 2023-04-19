507d4fe914d181d5cf665f1f96ea78056f4ce1d5
권효진 <C:/Program Files/Git/irisnari22@gmail.com>
2023-04-19 11:42:05 +0900

    상품 등록 구현하기 - 엔티티 파일, pom.xml 의존성, DTO 클래스 추가

---
+++ src/main/java/com/celinecloset/dto/ItemImgDto.java
@@ -0,0 +1,28 @@
+package com.celinecloset.dto;
+
+import com.celinecloset.entity.ItemImg;
+import lombok.Getter;
+import lombok.Setter;
+import org.modelmapper.ModelMapper;
+
+@Getter
+@Setter
+public class ItemImgDto {
+
+    private Long id;
+
+    private String imgName;
+
+    private String oriImgName;
+
+    private String imgUrl;
+
+    private String repImgYn;
+
+    private static ModelMapper modelMapper = new ModelMapper();
+
+    public static ItemImgDto of(ItemImg itemImg){
+        return modelMapper.map(itemImg,ItemImgDto.class);
+    }
+
+}
