4153dec304f237606ceae2e29f1284e539d15e29
권효진 <C:/Program Files/Git/irisnari22@gmail.com>
2023-04-20 10:54:35 +0900

    이미지 파일 등록 설정

---
+++ src/main/java/com/celinecloset/repository/ItemImgRepository.java
@@ -0,0 +1,8 @@
+package com.celinecloset.repository;
+
+import com.celinecloset.entity.ItemImg;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+
+}
