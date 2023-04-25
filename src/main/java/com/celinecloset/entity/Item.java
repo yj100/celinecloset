8898653b193e4e6b35b7efb2c1fbed73e2a1514c
권효진 <C:/Program Files/Git/irisnari22@gmail.com>
2023-04-25 11:06:17 +0900

    extends BaseEntity 추가

--- src/main/java/com/celinecloset/entity/Item.java
+++ src/main/java/com/celinecloset/entity/Item.java
@@ -15,7 +15,7 @@ import java.util.List;
 @Getter
 @Setter
 @ToString
-public class Item {
+public class Item extends BaseEntity {
 
     @ManyToMany
     @JoinTable(
--- src/main/java/com/celinecloset/entity/OrderItem.java
+++ src/main/java/com/celinecloset/entity/OrderItem.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @Setter
-public class OrderItem {
+public class OrderItem extends BaseEntity {
 
     @Id
     @GeneratedValue
