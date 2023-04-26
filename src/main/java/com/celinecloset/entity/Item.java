00756fed9810758192d40a2fa3a05b5ab395debe
권효진 <C:/Program Files/Git/irisnari22@gmail.com>
2023-04-26 12:08:19 +0900

    주문 기능 구현하기

--- src/main/java/com/celinecloset/entity/Item.java
+++ src/main/java/com/celinecloset/entity/Item.java
@@ -1,6 +1,7 @@
 package com.celinecloset.entity;
 
 import com.celinecloset.constant.ItemSellStatus;
+import com.celinecloset.exception.OutOfStockException;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -57,6 +58,15 @@ public class Item extends BaseEntity {
         this.stockNumber = itemFormDto.getStockNumber();
         this.itemDetail = itemFormDto.getItemDetail();
         this.itemSellStatus = itemFormDto.getItemSellStatus();
+    }
+
+    public void removeStock(int stockNumber){
+        int restStock = this.stockNumber - stockNumber;
+        if(restStock<0){
+            throw new OutOfStockException("상품의 재고가 부족 합니다.(현재 재고 수량 :" + this.stockNumber + ")");
+        }
+        this.stockNumber = restStock;
+
     }
 
 }
