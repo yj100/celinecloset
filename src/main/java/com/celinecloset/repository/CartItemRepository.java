f3f20d2c03aa03ad96e643caa05b4422dea47197
조연지 <gdduswl96@gmail.com>
2023-05-04 11:28:29 +0900

    장바구니 담기 구현하기

---
+++ src/main/java/com/celinecloset/repository/CartItemRepository.java
@@ -0,0 +1,8 @@
+package com.celinecloset.repository;
+
+import com.celinecloset.entity.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    CartItem findByCartIdAndItemId(Long cartId, Long itemId);
+}
---
+++ src/main/java/com/celinecloset/service/CartService.java
@@ -0,0 +1,53 @@
+package com.celinecloset.service;
+
+import com.celinecloset.dto.CartItemDto;
+import com.celinecloset.entity.Cart;
+import com.celinecloset.entity.CartItem;
+import com.celinecloset.entity.Item;
+import com.celinecloset.entity.Member;
+import com.celinecloset.repository.CartItemRepository;
+import com.celinecloset.repository.CartRepository;
+import com.celinecloset.repository.ItemRepository;
+import com.celinecloset.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class CartService {
+
+    private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
+    private final CartRepository cartRepository;
+    private final CartItemRepository cartItemRepository;
+    private final OrderService orderService;
+
+    public Long addCart(CartItemDto cartItemDto, String email) {
+
+        Item item = itemRepository.findById(cartItemDto.getItemId())
+                .orElseThrow(EntityNotFoundException::new);
+        Member member = memberRepository.findByEmail(email);
+
+        Cart cart = cartRepository.findByMemberId(member.getId());
+        if (cart == null) {
+            cart = Cart.createCart(member);
+            cartRepository.save(cart);
+        }
+
+        CartItem savedCartItem = cartItemRepository.findByCartIdAndItemId(cart.getId(), item.getId());
+
+        if (savedCartItem != null) {
+            savedCartItem.addCount(cartItemDto.getCount());
+            return savedCartItem.getId();
+        } else {
+            CartItem cartItem = CartItem.createCartItem(cart, item, cartItemDto.getCount());
+            cartItemRepository.save(cartItem);
+            return cartItem.getId();
+        }
+    }
+}
+
