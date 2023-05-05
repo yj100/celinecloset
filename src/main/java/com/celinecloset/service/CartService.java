e5c3eabc33244fa80e6f196f2b6e69d5aa16e459
조연지 <gdduswl96@gmail.com>
2023-05-05 11:15:29 +0900

    장바구니 수량 변경

--- src/main/java/com/celinecloset/service/CartService.java
+++ src/main/java/com/celinecloset/service/CartService.java
@@ -18,6 +18,7 @@ import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.thymeleaf.util.StringUtils;
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -68,6 +69,27 @@ public class CartService {
 
         return cartDetailDtoList;
 
+    }
+
+    @Transactional(readOnly = true)
+    public boolean validateCartItem(Long cartItemId, String email){
+        Member curMember = memberRepository.findByEmail(email);
+        CartItem cartItem = cartItemRepository.findById(cartItemId)
+                .orElseThrow(EntityNotFoundException::new);
+        Member savedMember = cartItem.getCart().getMember();
+
+        if(!StringUtils.equals(curMember.getEmail(), savedMember.getEmail())){
+            return false;
+        }
+
+        return true;
+    }
+
+    public void updateCartItemCount(Long cartItemId, int count){
+        CartItem cartItem = cartItemRepository.findById(cartItemId)
+                .orElseThrow(EntityNotFoundException::new);
+
+        cartItem.updateCount(count);
     }
 
 }
